7c3e7e324d7c5864a5a6ee70e4e10de8295782f4
LNooteboom <l.nooteboom@gmail.com>
2020-02-21 11:45:36 +0100

    Added room 2

---
+++ Assets/SceneTeleporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTeleporter : MonoBehaviour
+{
+	public string destination = "SampleScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnMouseDown()
+	{
+		SceneManager.LoadScene(destination);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
